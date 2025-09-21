d7782a7adfb8709e3968e9663f367cd0aab92cd6
A. Schueler <git@lakur.tech>
2025-09-21 17:10:44 +0200

    feat: refresh app on preloadError

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,10 @@ import { firebaseApp } from './firebase';
 import router from './router';
 import './assets/index.css';
 
+window.addEventListener("vite:preloadError", () => {
+  window.location.reload();
+});
+
 const app = createApp(App);
 app.use(router);
 
